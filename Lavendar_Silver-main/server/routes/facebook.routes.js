353fb99c9b75c25b60d05ec6008d70a0b944f334
DEVILSMOKERKS <141413927+DEVILSMOKERKS@users.noreply.github.com>
2026-02-14 22:42:10 +0530

    Delete Lavendar_Silver-main directory
    delete message

--- Lavendar_Silver-main/server/routes/facebook.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const facebook = require('../controllers/facebook.controller');
-
-router.post('/login', facebook.facebookLogin);
-router.post('/signup', facebook.facebookSignup);
-router.post('/admin/login', facebook.facebookAdminLogin);
-
-module.exports = router;
